60d226b5eb266c6b2a8ff214058bfd9a8a5225af
BERIN02 <berin698@gmail.com>
2022-07-19 21:51:15 -0700

    Berin's code

--- src/test/java/org/sample/GitSample.java
+++ src/test/java/org/sample/GitSample.java
@@ -1,5 +1,10 @@
 package org.sample;
 
 public class GitSample {
+	public static void main(String[] args) {
+		System.out.println("text A");
+		System.out.println("text B");
+		System.out.println("text C");
+	}
 
 }
